05cf852918e61e01a9537eba0ca1cfd06ee75966
obifade <obifade@users.noreply.github.com>
2016-10-11 21:41:26 +0100

    Add days to uptime

--- commands/misc/uptime.js
+++ commands/misc/uptime.js
@@ -2,8 +2,7 @@
 const bot = require('./../../index.js');
 
 bot.registerCommand('uptime', (msg, args) => {
-    let date = new Date(bot.uptime);
-    return `**${msg.author.username}**: ${date.getHours()}h : ${date.getMinutes()}m : ${date.getSeconds()}s`;
+    return `**${msg.author.username}**, ${Math.floor(bot.uptime / 864000000)}d : ${Math.floor((bot.uptime / 3600000) % 24)}h : ${Math.floor((bot.uptime / 60000) % 60)}m : ${Math.floor((bot.uptime / 1000) % 60)}s`;
 }, {
     caseInsensitive: true,
     deleteCommand: true,
