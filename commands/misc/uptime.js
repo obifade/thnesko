efe12f13bdde26f3420acb7f64f63d3967484708
obifade <obifade@users.noreply.github.com>
2016-10-13 01:00:41 +0100

    Fix uptime

--- commands/misc/uptime.js
+++ commands/misc/uptime.js
@@ -2,7 +2,7 @@
 const bot = require('./../../index.js');
 
 bot.registerCommand('uptime', (msg, args) => {
-    return `**${msg.author.username}**, ${Math.floor(bot.uptime / 864000000)}d : ${Math.floor((bot.uptime / 3600000) % 24)}h : ${Math.floor((bot.uptime / 60000) % 60)}m : ${Math.floor((bot.uptime / 1000) % 60)}s`;
+    return `**${msg.author.username}**, ${Math.floor(bot.uptime / 86400000)}d : ${Math.floor((bot.uptime / 3600000) % 24)}h : ${Math.floor((bot.uptime / 60000) % 60)}m : ${Math.floor((bot.uptime / 1000) % 60)}s`;
 }, {
     caseInsensitive: true,
     deleteCommand: true,
