5d6d97a7a6b8b4f8b4da83e2c3ebd82d65499342
obifade <obifade@users.noreply.github.com>
2016-10-13 15:28:08 +0100

    :eyes:

--- commands/misc/uptime.js
+++ commands/misc/uptime.js
@@ -3,7 +3,14 @@ const bot = require('./../../index.js');
 const os = require('os');
 
 bot.registerCommand('uptime', (msg, args) => {
-    return `**${msg.author.username}**, ${Math.floor(bot.uptime / 86400000)}d : ${Math.floor((bot.uptime / 3600000) % 24)}h : ${Math.floor((bot.uptime / 60000) % 60)}m : ${Math.floor((bot.uptime / 1000) % 60)}s`;
+    let uptime = bot.uptime / 1000;
+    let response = `${Math.floor(uptime / 86400)}d : `;
+    uptime %= 86400;
+    response += `${Math.floor(uptime / 3600)}h : `;
+    uptime %= 3600;
+    response += `${Math.floor(uptime / 60)}m : `;
+    response += `${Math.floor(uptime % 60)}s`;
+    return `**${msg.author.username}**, here is my uptime - ${response}`;
 }, {
     caseInsensitive: true,
     deleteCommand: true,
@@ -13,7 +20,14 @@ bot.registerCommand('uptime', (msg, args) => {
 });
 
 bot.commands.uptime.registerSubcommand('server', (msg, args) => {
-    return `**${msg.author.username}**, ${Math.floor(os.uptime / 86400)}d : ${Math.floor((os.uptime / 3600) % 24)}h : ${Math.floor((os.uptime / 60) % 60)}m : ${Math.floor((os.uptime / 60) % 60)}s`;
+    let uptime = os.uptime();
+    let response = `${Math.floor(uptime / 86400)}d : `;
+    uptime %= 86400;
+    response += `${Math.floor(uptime / 3600)}h : `;
+    uptime %= 3600;
+    response += `${Math.floor(uptime / 60)}m : `;
+    response += `${Math.floor(uptime % 60)}s`;
+    return `**${msg.author.username}**, here is the uptime of the server - ${response}`;
 }, {
     caseInsensitive: true,
     description: 'uptime of the server.',
